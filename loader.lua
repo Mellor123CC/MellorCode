692d67fd18219c435ebe21fcdee8e664fdaf7361
Mellor123CC <116329769+Mellor123CC@users.noreply.github.com>
2022-10-21 22:07:14 +0500

    Add files via upload

---
+++ loader.lua
@@ -0,0 +1,12 @@
+local whitelistecheck = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/White-List/main/DataBase", true))()
+
+userids = {
+[3973824842] = true
+}
+return userids
+
+if whitelistecheck[game:service('Players').LocalPlayer.UserId] then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Mellor123CC/MellorCode/main/mellorcode.lua%22))()
+else
+  game:service('Players').LocalPlayer:Kick('Bruh')
+end
