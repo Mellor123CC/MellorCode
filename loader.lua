0c7fab9e96f2df629426f7f8425d19912f3155ec
Mellor123CC <116329769+Mellor123CC@users.noreply.github.com>
2022-10-22 20:16:29 +0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,13 +1,1 @@
-local whitelistecheck = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/White-List/main/DataBase", true))()
-
-userids = {
-[3973824842] = true,
-[3795008910] = true
-}
-return userids
-
-if whitelistecheck[game:service('Players').LocalPlayer.UserId] then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Mellor123CC/MellorCode/main/mellorcode.lua"))()
-else
-  game:service('Players').LocalPlayer:Kick('Bruh')
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Mellor123CC/MellorCode/main/mellorcode.lua"))()
