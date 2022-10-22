f35523df4574db3c3f130c49af32b1c6defaba5e
Mellor123CC <116329769+Mellor123CC@users.noreply.github.com>
2022-10-22 20:43:20 +0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Mellor123CC/MellorCode/main/mellorcode.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Mellor123CC/MellorCode/main/MellorCode.lua"))()
