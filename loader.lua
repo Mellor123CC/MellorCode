7afbc1d3c4b5f961f023a2765b5f16f71d2503f9
Mellor <116329769+Mellor123CC@users.noreply.github.com>
2022-11-06 20:36:43 +0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,11 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Mellor123CC/MellorCode/main/MellorCode.lua"))()
+if game.PlaceId == 6516141723 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Mellor123CC/MellorCode/main/Doors.lua", true))()
+
+if game.PlaceId == 142823291 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Mellor123CC/MellorCode/main/Murder.lua", true))()
+
+if game.PlaceId == 155615604 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Mellor123CC/MellorCode/main/Prison.lua", true))()
+
+if game.PlaceId == 1962086868 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Mellor123CC/MellorCode/main/TowerOfHell.lua", true))()
