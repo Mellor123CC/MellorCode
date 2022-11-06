d97502f4ab11b7791c77b9d46cd8a7f97cfaee8e
Mellor <116329769+Mellor123CC@users.noreply.github.com>
2022-11-06 20:55:30 +0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,11 +1,11 @@
-if game.PlaceId == 6516141723 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Mellor123CC/MellorCode/main/Doors.lua", true))()
+if game.PlaceId == 8884334497 then -- Doors
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Mellor123CC/MellorCode/main/Doors.lua"))()
+elseif game.PlaceId == 142823291 then -- Murder Mystery 2
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Mellor123CC/MellorCode/main/Murder.lua"))()
+elseif game.PlaceId == 155615604 then -- Prison Life 2
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Mellor123CC/MellorCode/main/Prison.lua"))()
+elseif game.PlaceId == 1962086868 then -- Tower of Hell 2
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Mellor123CC/MellorCode/main/TowerOfHell.lua"))()
 
-if game.PlaceId == 142823291 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Mellor123CC/MellorCode/main/Murder.lua", true))()
 
-if game.PlaceId == 155615604 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Mellor123CC/MellorCode/main/Prison.lua", true))()
-
-if game.PlaceId == 1962086868 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Mellor123CC/MellorCode/main/TowerOfHell.lua", true))()
+end
